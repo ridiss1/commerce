919c831cd47b70b272f4c3b1a1152938c2a243fc
Idriss Laouali <idrisslaouali@gmail.com>
2020-06-19 00:38:39 +0200

    microcommerce config

--- src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
+++ src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
@@ -2,6 +2,7 @@ package com.ecommerce.microcommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
